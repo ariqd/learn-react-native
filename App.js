a30b8e3ce5dc15faa29bcd9e9c31fde15407e852
Ariq Daffa <ariqdaffaathallah@gmail.com>
2020-10-07 17:44:04 +0700

    text input done

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
   const [name, setName] = useState("Ariq");
@@ -8,6 +8,20 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <Text>Enter name:</Text>
+      <TextInput
+        multiline
+        style={styles.input}
+        placeholder="e.g. John Doe"
+        onChangeText={(value) => setName(value)}
+      />
+      <Text>Enter age:</Text>
+      <TextInput 
+        style={styles.input}
+        keyboardType="numeric"
+        placeholder="e.g. 20"
+        onChangeText={(value) => setAge(value)}
+      />
       <Text>
         Name: {name}, Age: {age}
       </Text>
@@ -23,4 +37,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+    borderRadius: 10
+  }
 });
