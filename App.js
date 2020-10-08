7664e1461feebea3ff3170782655f1de0493bfde
Ariq Daffa <ariqdaffaathallah@gmail.com>
2020-10-08 14:35:07 +0700

    todoitem

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, View, Text, FlatList } from "react-native";
-import Header from './components/Header';
+import Header from "./components/Header";
+import TodoItem from "./components/TodoItem";
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -10,15 +11,21 @@ export default function App() {
     { text: "play nintendo switch", key: 3 },
   ]);
 
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key != key);
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Header/>
+      <Header />
       <View style={styles.content}>
         {/* todo form */}
         <View style={styles.list}>
           <FlatList
             data={todos}
-            renderItem={({ item }) => <Text>{item.text}</Text>}
+            renderItem={({ item }) => <TodoItem item={item} pressHandler={pressHandler} />}
           />
         </View>
       </View>
@@ -33,9 +40,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
   content: {
-    padding: 40
+    padding: 30,
   },
   list: {
     marginTop: 20,
-  }
+  },
 });
