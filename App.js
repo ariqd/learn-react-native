94e1a99566808035bb73e6251c5c5d8d53e74db0
Ariq Daffa <ariqdaffaathallah@gmail.com>
2020-10-08 15:51:18 +0700

    todo form

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import React, { useState } from "react";
 import { StyleSheet, View, Text, FlatList } from "react-native";
 import Header from "./components/Header";
 import TodoItem from "./components/TodoItem";
+import TodoForm from "./components/TodoForm";
 
 export default function App() {
   const [todos, setTodos] = useState([
-    { text: "buy coffee", key: 1 },
-    { text: "create an app", key: 2 },
-    { text: "play nintendo switch", key: 3 },
+    { text: "buy coffee", key: '1' },
+    { text: "create an app", key: '2' },
+    { text: "play nintendo switch", key: '3' },
+    { text: "go shopping", key: '4' },
   ]);
 
   const pressHandler = (key) => {
@@ -17,15 +19,23 @@ export default function App() {
     });
   };
 
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
-        {/* todo form */}
+        <TodoForm submitHandler={submitHandler} />
         <View style={styles.list}>
           <FlatList
             data={todos}
-            renderItem={({ item }) => <TodoItem item={item} pressHandler={pressHandler} />}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
           />
         </View>
       </View>
