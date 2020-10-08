82715c50fd5b648fb07c6063f782917cb69ec031
Ariq Daffa <ariqdaffaathallah@gmail.com>
2020-10-08 13:56:20 +0700

    flatlist

--- App.js
+++ App.js
@@ -1,30 +1,40 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 export default function App() {
   const [people, setPeople] = useState([
-    { name: "ariq", key: 1 },
-    { name: "daffa", key: 2 },
-    { name: "athallah", key: 3 },
-    { name: "putra", key: 4 },
-    { name: "harahap", key: 5 },
-    { name: "indri", key: 6 },
-    { name: "athar", key: 7 },
-    { name: "jokowi", key: 8 },
+    { name: "ariq", id: 1 },
+    { name: "daffa", id: 2 },
+    { name: "athallah", id: 3 },
+    { name: "putra", id: 4 },
+    { name: "harahap", id: 5 },
+    { name: "indri", id: 6 },
+    { name: "athar", id: 7 },
+    { name: "jokowi", id: 8 },
+    { name: "marco", id: 9 },
+    { name: "eren", id: 10 },
   ]);
 
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        {people.map((person) => (
-          <View key={person.key}>
-            <Text style={styles.item}>{person.name}</Text>
-          </View>
-        ))}
-        <StatusBar style="auto" />
-      </View>
-    </ScrollView>
+    <View style={styles.container}>
+      <FlatList
+        data={people}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+      />
+      <StatusBar style="auto" />
+      {/* <ScrollView>
+        <View style={styles.container}>
+          {people.map((person) => (
+            <View key={person.key}>
+              <Text style={styles.item}>{person.name}</Text>
+            </View>
+          ))}
+        </View>
+      </ScrollView> */}
+    </View>
   );
 }
 
@@ -42,5 +52,6 @@ const styles = StyleSheet.create({
     padding: 30,
     backgroundColor: "pink",
     fontSize: 24,
+    marginHorizontal: 10,
   },
 });
